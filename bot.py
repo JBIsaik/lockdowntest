d26cc63317fc32baa3a5b673d2d49eea5783d57a
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-07-31 09:41:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ lines = open(r'spotify.txt').read().splitlines()
 
 @bot.event
 async def on_ready():
-    print("The bot is online!")
-    await bot.change_presence(game=discord.Game(name='Type $uplay'))
-
+    print('The bot is online!')
+    await bot.change_presence(game=discord.Game(name='Type $minecraft'))
+   
 @commands.cooldown(1, 10, commands.BucketType.user)
 @bot.command(pass_context=True)
 async def uplay(ctx):
@@ -19,17 +19,17 @@ async def uplay(ctx):
     userID = ctx.message.author.id
 
     if ctx.message.server:
-        await bot.delete_message(ctx.message)
-    myline = random.choice(lines)
-    split = myline.partition(":")
-    
-    embed=discord.Embed(title="Uplay Account", color=0xf45eff)
-    embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
-    embed.add_field(name="Email:", value=split[0], inline=False)
-    embed.add_field(name="Password:", value=split[2], inline=False)
-    await bot.send_message(ctx.message.author, embed=embed)
-    
+        myline = random.choice(lines)
+        split = myline.partition(":")
+       
+        embed=discord.Embed(title="Uplay Account", color=0xf45eff)
+        embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
+        embed.add_field(name="Email:", value=split[0], inline=False)
+        embed.add_field(name="Password:", value=split[2], inline=False)
+        await bot.send_message(ctx.message.author, embed=embed)
 
-    print("{} Typed $uplay".format(userName))
+        print("{} Typed $uplay".format(userName))
+
+        client = discord.Client()
 
 bot.run("NDcyMzU0NjA0ODMxNzM1ODE4.Dj3LlQ.Uq7P7i9IaSZ7fNBocU0-_za1tbM")
